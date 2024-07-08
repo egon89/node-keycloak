b0c7d2d8042bd9da475c2e27824a9d4991ba4789
everton_goncalves <everton.goncalves.89@gmail.com>
2024-07-08 15:42:18 -0300

    refactor: change to aes-128-cbc algorithm

--- authorization-code-flow/src/crypto.ts
+++ authorization-code-flow/src/crypto.ts
@@ -2,8 +2,8 @@ import crypto from 'crypto';
 
 export const encrypt = (text: string, secret: string): string => {
     const iv = crypto.randomBytes(16);
-    const key = crypto.scryptSync(secret, 'salt', 32);
-    const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+    const key = crypto.scryptSync(secret, 'salt', 16);
+    const cipher = crypto.createCipheriv('aes-128-cbc', key, iv);
     let encrypted = cipher.update(text, 'utf8', 'hex');
     encrypted += cipher.final('hex');
 
@@ -13,9 +13,9 @@ export const encrypt = (text: string, secret: string): string => {
 export const decrypt = (encryptedText: string, secret: string): string => {
     const [ivHex, encrypted] = encryptedText.split(':');
     const iv = Buffer.from(ivHex, 'hex');
-    const key = crypto.scryptSync(secret, 'salt', 32);
+    const key = crypto.scryptSync(secret, 'salt', 16);
 
-    const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
+    const decipher = crypto.createDecipheriv('aes-128-cbc', key, iv);
     let decrypted = decipher.update(encrypted, 'hex', 'utf8');
     decrypted += decipher.final('utf8');
 
