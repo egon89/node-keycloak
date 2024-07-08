bce6889bb30a26005e78b571ee1d6539ab6e6bc5
everton_goncalves <everton.goncalves.89@gmail.com>
2024-07-08 10:26:41 -0300

    chore: change keycloak version

--- authorization-code-flow/src/index.ts
+++ authorization-code-flow/src/index.ts
@@ -57,6 +57,7 @@ app.get('/callback', async (req, res) => {
   res.json(result);
 });
 
+// create QR code with target URL using the token as query parameter
 app.post('/create-qr-code', async (req, res) => {
   const bearerToken = req.headers.authorization;
   const token = bearerToken?.split(' ')[1];
@@ -79,12 +80,13 @@ app.post('/create-qr-code', async (req, res) => {
   res.json({ qrCode });
 });
 
+// scan QR code and redirect to the URL
 app.get('/scan-qr-code', async (req, res) => { 
   const qrCodeContent = await decodeQR('./output/qr-code.png')
   console.log(`QR Code scanned and redirect to ${qrCodeContent.substring(0, 100)}...`);
-  await axios.get(qrCodeContent);
+  const { data } = await axios.get(qrCodeContent);
 
-  res.sendStatus(204);
+  res.json(data);
 });
 
 app.get('/pre-action', async (req, res) => {
@@ -93,25 +95,23 @@ app.get('/pre-action', async (req, res) => {
 
   // instropect token
   const bodyParams = new URLSearchParams({
-    token: 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJUbjFIam80cjNLNTRrdkhHRHV6NmxTMUs2eVIwNG5yZnlfNWlUNTVWanpVIn0.eyJleHAiOjE3MjAzMTkyMzAsImlhdCI6MTcyMDMxODkzMCwiYXV0aF90aW1lIjoxNzIwMzE4NDk3LCJqdGkiOiI3YjUyZWZkMi1kNzRhLTQwNDEtOTdlMi0zMWU0ZWYwNWM4YTkiLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwODAvcmVhbG1zL3BvYyIsImF1ZCI6InBvYyIsInN1YiI6IjNmNWMyYTE5LWVmMWItNDY2YS1hYmU0LTMzMmFjMjcyNjQyMCIsInR5cCI6IklEIiwiYXpwIjoicG9jIiwic2Vzc2lvbl9zdGF0ZSI6Ijc3ZDMzYWI2LTJlNzYtNDBmMC1iYzdlLWRlMjY4N2ZlYzVlOSIsImF0X2hhc2giOiJ6ME5kV2NnQ0VsNzBYU2ZncVNEX2xRIiwiYWNyIjoiMCIsInNpZCI6Ijc3ZDMzYWI2LTJlNzYtNDBmMC1iYzdlLWRlMjY4N2ZlYzVlOSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwibmFtZSI6InVzZXIxIGRvZSIsInByZWZlcnJlZF91c2VybmFtZSI6InVzZXIxIiwiZ2l2ZW5fbmFtZSI6InVzZXIxIiwiZmFtaWx5X25hbWUiOiJkb2UiLCJlbWFpbCI6InVzZXIxQGdtYWlsLmNvbSJ9.k9aZAPtGYqP4JpU824AvjnHbkZ0YLvT5KYTXZze6tf_d3ijk4x8gGx-HKmjCkbB20-Exit-FlVJC8Gg0WZ5_xfmQoSlrIotwZEm7tAbqW0USjGG1JEn9F_pn5wEEYxFOHi4acYnawLNtcrjdR508MsHZom_4tZ2sMg-cCror3giXw-y4zN2JjA2j4ypHGl66e7sBqENLqc7sr2uCRYX-UZVL91d1hNTFSduYf46N1f0r5lMqdu2L8PfD89OIAk2nzyIDm0cpkeqSieogkwGJgQULO4wPY5wQpZpr2AmmSGGQhJPduItn0ydiCiktrG3PHBtLDfwe-KmnvOpvcZvF6Q',
+    token,
     client_id: clientId,
-    client_secret: '5y4VqRkFE1P6qXd7oiGw46eDOG5KhtOV',
+    client_secret: clientSecret,
   });
 
   const url = `http://${keycloakHost}/realms/${realm}/protocol/openid-connect/token/introspect`;
-  const response = await axios.post(url, bodyParams, {
+  const { data } = await axios.post(url, bodyParams, {
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
     },
   });
 
-  console.log(response.data);
-
-  res.sendStatus(204);
+  res.json(data);
 });
 
 app.post('/introspect', async (req, res) => {
-  const token = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJUbjFIam80cjNLNTRrdkhHRHV6NmxTMUs2eVIwNG5yZnlfNWlUNTVWanpVIn0.eyJleHAiOjE3MjAzMjkzOTgsImlhdCI6MTcyMDMyOTA5OCwiYXV0aF90aW1lIjoxNzIwMzI5MDk4LCJqdGkiOiJlYWY2N2Q4NC02NWM5LTQ1NmItODExNC1lZjI5NDFkNGZmYmIiLCJpc3MiOiJodHRwOi8vaG9zdC5kb2NrZXIuaW50ZXJuYWw6ODA4MC9yZWFsbXMvcG9jIiwiYXVkIjoiYWNjb3VudCIsInN1YiI6IjNmNWMyYTE5LWVmMWItNDY2YS1hYmU0LTMzMmFjMjcyNjQyMCIsInR5cCI6IkJlYXJlciIsImF6cCI6InBvYyIsInNlc3Npb25fc3RhdGUiOiI1ZmJjYzgzZC0wYzM4LTQ3ODAtYmIxNS01MjRkZGE0NzY3ODkiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbIioiXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbImRlZmF1bHQtcm9sZXMtcG9jIiwib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoib3BlbmlkIGVtYWlsIHByb2ZpbGUiLCJzaWQiOiI1ZmJjYzgzZC0wYzM4LTQ3ODAtYmIxNS01MjRkZGE0NzY3ODkiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsIm5hbWUiOiJ1c2VyMSBkb2UiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJ1c2VyMSIsImdpdmVuX25hbWUiOiJ1c2VyMSIsImZhbWlseV9uYW1lIjoiZG9lIiwiZW1haWwiOiJ1c2VyMUBnbWFpbC5jb20ifQ.SMbTzcGnvK4WQKGd1V-iUcU57fUG7eH7tXzPvMNhUTVnN1wrOQCZekOg6SdHs6ZKjICPJqV3h5djYJHenGjRd6aCY937poLqCUlwIgvGmAmbxkNEcMCHKtQy-8W2qBmN4QHZjFrsUyoFMtbNl8tprQ_FpIXjvLRJdbHxmtXy7qe9TqFSedwaK22WpEcSqPW4hXV9CHeFxBP4n_4dY8wtJVcDDZ5Et3YRKeYVd7t48ZqB4LHf7WOvQ0oeHsDgtp_gvjt_NJ24pJiUAiqRlNGMNhjaM5sMi1S0VSwREzeMaRkT02GtFxs8i6LRo-zg57g58svglfFBlgISExLXfYF0bg';
+  const token = '';
   console.log(`Introspect token: ${token?.substring(0, 100)}...`);
 
   const bodyParams = new URLSearchParams({
@@ -126,8 +126,6 @@ app.post('/introspect', async (req, res) => {
       'Content-Type': 'application/x-www-form-urlencoded',
     },
   });
-
-  console.log(response.data);
 
   res.json(response.data);
 });
